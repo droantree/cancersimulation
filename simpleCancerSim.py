8f52ab09329f6fd965045702ab9697905cb5d0d6
droantree <donal@roantree.com>
2016-11-25 23:39:00 +0000

    Finally got it working. Number of slots per day decreasing past the "adequate" number. Displays a nice table of results.

--- simpleCancerSim.py
+++ simpleCancerSim.py
@@ -40,11 +40,11 @@ Created on Wed Oct 26 18:52:50 2016
 
 import numpy as np
 import misc as gp
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 #Simulation parameters
-POPULATION = 1000000
-SLOTS_PER_DAY = 20  #number of appointment slots that can be allocated each day
+POPULATION = 500000
+SLOTS_PER_DAY = 2  #number of appointment slots that can be allocated each day
 DAYS_TO_RUN = 365 * 5  # years
 CANCER_START_DAILY_INCIDENCE_RATE = float(1) / (2 * 70 * 365)  #corresponds to person having a 50/50 chance of developing cancer in their life, avg lifespan 70 years
 
@@ -70,7 +70,7 @@ class Sufferer:
     def cancerStage(self, day):
         return day - self.dayCancerStarted        
         
-    def progressOneDay(self, today):
+    def progressOneDay(self, today, simResults):
         #3 cases - no appt yet (because cancer not noticed)
         #        - waiting for an appointment
         #        - had an appt and either success or failure
@@ -79,6 +79,9 @@ class Sufferer:
             suffererNoticesCancerToday = self.isNoticed(cancerStageToday)
             if suffererNoticesCancerToday:
                 self.appt = self.apptSchedule.getNextApptSlot(today)
+                #print "appt day=" + str(self.appt) + ", today=" + str(int(today))
+                waitingDays = self.appt - today
+                simResults.addApptSet(waitingDays, cancerStageToday + waitingDays)
             #Note: if the sufferer does not notice the cancer there is no progress to process
         elif today < self.appt:
             pass #sufferer continues to wait for their appointment
@@ -127,6 +130,7 @@ class ApptSchedule:
             self.resetNextApptDayToDay(today)
         nextSlotDay = self.nextApptDay
         self.decrementSlotsAvailableNextApptDay()
+        #print "(Slots per day=" + str(int(SLOTS_PER_DAY)) + ") Next slot on day=" + str(int(today)) + ", next appt day=" + str(int(self.nextApptDay)) + ", appts remaining next day=" + str(int(self.apptsRemainingNextApptDay))
         return nextSlotDay
 ######## End of class ApptSchedule
         
@@ -144,11 +148,11 @@ class Model:
         numberOfNewCancerSuffers = numberOfNewCancerStartsToday()
         self.createNewSuffers(numberOfNewCancerSuffers) 
         #go through all sufferers and "progress" them to today
-        sufferersTreated = [] #ercord those sufferers that have been treated so they can be removed later
+        sufferersTreated = [] #record those sufferers that have been treated so they can be removed later
         for sufferer in self.sufferers:
-            sufferer.progressOneDay(self.today)
+            sufferer.progressOneDay(self.today, self.simResults)
             if sufferer.hasBeenTreated():
-                sufferersTreated.append(sufferer) #don't remove a sufferer from self.sufferers because that would interfere with the for loop 
+                sufferersTreated.append(sufferer) #don't remove a sufferer from self.sufferers here because that would interfere with the for loop 
                 if sufferer.isStatusFailed():
                     self.simResults.addDeath(self.today)
                 else:
@@ -167,12 +171,34 @@ class ModelResults:
     def __init__(self):
         self.deaths = np.zeros(DAYS_TO_RUN + 1)
         self.cured = np.zeros(DAYS_TO_RUN + 1)
+        #from these next 3 we can calculate avg days to wait for an appt and avg stage when treated
+        self.apptsWaitingDaysTotal = 0
+        self.apptsTotal = 0
+        self.treatmentStageTotalDays = 0
         
     def addDeath(self, day):
         self.deaths[day] += 1
         
+    def addApptSet(self, daysToWait, stageAtAppt):
+        self.apptsWaitingDaysTotal += daysToWait
+        self.treatmentStageTotalDays += stageAtAppt
+        self.apptsTotal += 1
+
     def addCure(self, day):
         self.cured[day] += 1
+        
+    def avgApptWaitDays(self):
+        if self.apptsTotal == 0:
+            return "--"
+        else:
+            return float(self.apptsWaitingDaysTotal) / float(self.apptsTotal)
+
+    def avgStageTreated(self):
+        if self.apptsTotal == 0:
+            return "--"
+        else:
+            return float(self.treatmentStageTotalDays) / float(self.apptsTotal)
+
 
 def numberOfNewCancerStartsToday():
     #Assume poisson process with mean rate*population.
@@ -187,16 +213,12 @@ def randomUniformFloat(min, max):
     return np.random.random_sample() * (max - min) + min
 
 #Finally, actually run the model:
-slotsPerDayList = []
-deathsList = []
-for slotsPerDay in range(18, 22):
+print "{:>10}  {:>10}  {:>10}  {:>10}".format("slots", "deaths", "avg waits", "avg stage")
+for SLOTS_PER_DAY in reversed(range(5, 16)):
     simResults = ModelResults();
     simModel = Model(simResults);
     for day in range(DAYS_TO_RUN):
         simModel.progressToNextDay()
-    deaths = np.sum(simResults.deaths)
-    slotsPerDayList.append(slotsPerDay)
-    deathsList.append(deaths)
-
-plt.plot(slotsPerDayList, deathsList)
-plt.show()
+    deaths = int(np.sum(simResults.deaths))
+    print "{:>10}  {:>10}  {:>10.2f}  {:>10.2f}".format(SLOTS_PER_DAY, deaths, simResults.avgApptWaitDays(), simResults.avgStageTreated())
+    
